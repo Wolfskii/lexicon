9348f84f965fe74d5b62a2f160ff32ac41a577a6
Dawid <dawid.kaleta@outlook.com>
2022-03-24 13:54:20 +0100

    Add beginning of calculator-menu

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,45 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool continueMenu = true;
+
+            while (continueMenu)
+            {
+                Console.Clear();
+                ShowMenu();
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+
+                        Console.WriteLine("1");
+                        break;
+
+                    case "2":
+
+                        Console.WriteLine("2");
+                        break;
+
+                    case "3":
+
+                        Console.WriteLine("3");
+                        break;
+
+                    case "4":
+
+                        Console.WriteLine("4");
+                        break;
+
+                    case "0":
+
+                        Console.WriteLine("0");
+                        continueMenu = false;
+                        break;
+                }
+            }
+            
 
             // Meny (som forsätter till avstängning)
             // Addition
@@ -15,5 +53,18 @@ namespace Calculator
             // Division
             // Avsluta applikation
         }
+
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Miniräknare:");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraktion");
+            Console.WriteLine("3. Multiplikation");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("0. Avsluta");
+        }
+
+
     }
 }
